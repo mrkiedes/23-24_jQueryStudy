00c624b8742f370cf30366fc504356cf8f5ad9e2
mrkiedes <akiedes@vtsd.com>
2024-01-18 11:43:08 -0500

    Old test Application

--- oldTest/assets/javascript/task2.js
+++ oldTest/assets/javascript/task2.js
@@ -1,1 +1,18 @@
 /* Put code for the task below */
+$(document).ready(function(){
+    /*$("#red").click(function(){
+       $(".table").css("background-color", "red");
+    });
+    $("#blue").click(function(){
+        $(".table").css("background-color", "blue");
+    });
+    $("#green").click(function(){
+        $(".table").css("background-color", "green");
+    });*/
+
+    $(".color").click(function(){
+       $(".table").css("background-color", $(this).val());
+    });
+
+
+});
